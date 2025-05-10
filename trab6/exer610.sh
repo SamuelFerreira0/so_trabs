02d046aa029787166c82f429083cfc2538367bb5
SamuelFerreira0 <samuel.ferreira6002@gmail.com>
2025-05-10 19:15:34 +0100

    1005_19:15

--- trab6/exer610.sh
+++ trab6/exer610.sh
@@ -1,15 +1,18 @@
 #Indica a shell que vai executar o script
 #!/bin/bash
- 
+
 clear
 
-echo "Vamos calcular a expressão (C=(A+B-B*24+A*10)"
-echo "A="
+echo "Vamos calcular a expressão: (A + B - B * 24 + A * 10)"
+echo -n "A= "
 read A
+echo -n "B= "
+read B
 
-let "C=(A+B-B*24+A*10)"
+C=$((A + B - B * 24 + A * 10))
+
 echo "O resultado é igual a $C"
- 
-sleep 1
 
-exit 0 # termina com sucesso
+sleep 1
+exit 0
+
